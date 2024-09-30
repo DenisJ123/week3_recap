f4b4198b8646cc45fe243f2f1f0481f56da578e1
Denis <G00403126@atu.ie>
2024-09-30 12:54:17 +0100

    Controller

---
+++ src/main/java/ie/atu/week3/Controller.java
@@ -0,0 +1,25 @@
+package ie.atu.week3;
+
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@RestController
+@RequestMapping("/products")
+public class Controller {
+    private List<Product> products = new ArrayList<>();
+    Service service = new Service();
+
+    @GetMapping("/get")
+    public List<Product> getProducts() {
+        return service.getProductList();
+    }
+
+    @PostMapping("/add")
+    public String addProduct(@RequestBody Product product) {
+        products.add(product);
+        return service.addProduct(product);
+    }
+
+}
